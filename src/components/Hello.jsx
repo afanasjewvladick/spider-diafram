40aa0d391666ea173bf693b31de8a10145d9233f
Vladick Afanasjew <afanasjew.vladick@yandex.ru>
2019-11-10 23:45:54 +0300

    eslint + вырезано лишнее

---
+++ src/components/Hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Hello = () => (
+  <div>
+    <p>Hello world</p>
+  </div>
+);
+
+export default Hello;
---
+++ src/pages/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from '../components/Hello.jsx';
+
+const App = () => (
+  <div>
+    <Hello />
+    <Hello />
+    <Hello />
+  </div>
+);
+
+export default App;
