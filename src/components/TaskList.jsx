b444fc5145014fa15bf8c559aa34de708718ec0e
Vladick Afanasjew <afanasjew.vladick@yandex.ru>
2019-11-13 22:34:18 +0300

    Добавление характеристик в реактивный обьект.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,48 +1,48 @@
-// import React from 'react';
 import React, { useState } from 'react';
 import './TaskList.scss';
 
-const lists = [
-  { id: 0, name: 'Test', value: 20 },
-];
-
-const TaskList = () => (
-  <div className="main-task-list">
-    <TaskListHeader />
-    <TaskListBody />
-    <CreateTaskButton />
-  </div>
-);
-
-const TaskListHeader = () => (
-  <div className="main-task-list-header">
-    <div style={{ color: 'white' }}>Список характеристик</div>
-  </div>
-);
-
-const TaskListBody = () => (
-  <div className="main-task-list-body">
-    <div className="main-task-list-body-name">
-      <div>{lists.map((item) => <div key={item.id}>{item.name}</div>)}</div>
+const App = () => {
+  const [lists, setList] = useState(
+    [
+      { id: 0, name: 'Test', value: 20 },
+    ],
+  );
+  const TaskListHeader = () => (
+    <div className="main-task-list-header">
+      <div style={{ color: 'white' }}>Список характеристик</div>
     </div>
-  </div>
-);
+  );
 
-const CreateTaskButton = () => (
-  <button
-    type="button"
-    className="create-button"
-    onClick={createTask}
-  >
-    Создать
-  </button>
-);
+  const TaskListBody = () => (
+    <div>
+      {lists.map((item) => <div className="main-task-list-body" key={item.id}>{item.name}</div>)}
+    </div>
+  );
 
-const createTask = () => (
-  lists.push({ id: 1, name: 'Test 2', value: 22 }),
-  // TaskListBody(),
-  console.log('lists', lists)
-);
+  const createTask = () => {
+    lists.push({ id: 1, name: 'Test 2', value: 22 });
+    // Важно! Иначе не работает...
+    setList([...lists]);
+  };
+
+  const CreateTaskButton = () => (
+    <button
+      type="button"
+      className="create-button"
+      onClick={createTask}
+    >
+      Создать
+    </button>
+  );
+
+  return (
+    <div className="main-task-list">
+      <TaskListHeader />
+      <TaskListBody />
+      <CreateTaskButton />
+    </div>
+  );
+};
 
 
-export default TaskList;
+export default App;
